74daa4427419c48de6c9cc45809aed93f43fa8d8
Chouteau Perry <perry.chouteau@epitech.eu>
2025-06-13 06:31:51 +0200

    fix(typo) :pencil2: GetXxxx to getXxx

--- includes/IModule.hpp
+++ includes/IModule.hpp
@@ -9,9 +9,9 @@ class IModule {
 
         virtual ~IModule() = default;
 
-        virtual const std::string GetType() const = 0;
+        virtual const std::string getType() const = 0;
 
-        virtual const std::string GetName() const = 0;
+        virtual const std::string getName() const = 0;
 };
 
 #endif // MODULE_HPP
